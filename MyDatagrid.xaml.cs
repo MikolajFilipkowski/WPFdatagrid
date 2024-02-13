03b16d0e9ea1df03b95890eddb8d5c28e0faf4db
mikolajfilipkowski <mikolajfilipkowski000@gmail.com>
2024-02-13 09:40:24 +0100

    Update datagrid

--- MyDatagrid.xaml.cs
+++ MyDatagrid.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,7 +23,7 @@ namespace WPFdatagrid
     public partial class MyDatagrid : Window
     {
         SQLHandler sqlHandler;
-        List<MyUser> users;
+        List<MyUser> users { get; set; }
         public MyDatagrid(SQLHandler handler)
         {
             InitializeComponent();
@@ -34,6 +35,59 @@ namespace WPFdatagrid
         {
             myData.ItemsSource = users;
             //sqlHandler.InsertData(sqlHandler.databaseRepresentation);
+        }
+
+        void myData_CellEditEnding(object sender, DataGridCellEditEndingEventArgs e)
+        {
+            if (e.EditAction == DataGridEditAction.Commit)
+            {
+                var column = e.Column as DataGridBoundColumn;
+                if (column != null)
+                {
+                    var bindingPath = ((Binding)column.Binding).Path.Path;
+                    if (bindingPath == "Id")
+                    {
+                       
+                        int rowIndex = e.Row.GetIndex();
+                        var el = (TextBox)e.EditingElement;
+                        // rowIndex has the row index
+                        // bindingPath has the column's binding
+                        // el.Text has the new, user-entered value
+
+                        try
+                        {
+                            foreach (var user in users)
+                            {
+                                if (user.Id == Int32.Parse(el.Text))
+                                {
+                                    el.Text = users[rowIndex].Id.ToString();
+                                    break;
+                                }
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            Trace.TraceError(ex.Message);
+                        }
+                        finally
+                        {
+                            Trace.WriteLine(users[0]);
+                        }
+                    }
+                }
+            }
+            if (!e.Row.IsEditing)
+            {
+                this.myData.Items.Refresh();
+            }
+        }
+
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            sqlHandler.TruncateData();
+            sqlHandler.InsertData(users);
+            users = sqlHandler.ReadData();
+            myData.Items.Refresh();
         }
     }
 }
